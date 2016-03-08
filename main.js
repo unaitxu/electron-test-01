1cf8421024ea3430406e3987e7d28a4aaf5f777c
Unai <unaimartinezrodriguez@gmail.com>
2016-03-08 23:35:41 +0100

    Frameless and draggable

--- main.js
+++ main.js
@@ -7,7 +7,8 @@ var mainWindow = null;
 app.on('ready', function() {
   mainWindow = new BrowserWindow({
     height: 400,
-    width: 400
+    width: 400,
+    frame: false
   });
 
   mainWindow.loadUrl('file://' + __dirname + '/app/index.html');
