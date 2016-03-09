1caa80e610783a5d496c80972534e86401fa0c9e
Unai <unaimartinezrodriguez@gmail.com>
2016-03-10 00:05:16 +0100

    Little cleaning and css changes

--- app/js/index.js
+++ app/js/index.js
@@ -1,8 +1,7 @@
 $(function() {
-  $('body').on( 'click', '.flex-item', function (e) {
-    console.log('e');
+  $('body').on( 'click', '.flex-item', function () {
     var element = $(this).children()[0];
     $(element).text('Clicked!');
-    setTimeout(function() { $(element).text(element.id.split('-')[1]); }, 2000);
+    setTimeout(function() { $(element).text(element.id.split('-')[1]); }, 1000);
   });
 });
