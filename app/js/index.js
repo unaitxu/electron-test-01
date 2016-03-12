e9321271bb933317fc2909f572b135dd584b159d
Unai <unaimartinezrodriguez@gmail.com>
2016-03-12 18:16:38 +0100

    Add numbers and reset the count

--- app/js/index.js
+++ app/js/index.js
@@ -1,7 +1,15 @@
 $(function() {
+  var counter = 0;
   $('body').on( 'click', '.flex-item', function () {
     var element = $(this).children()[0];
-    $(element).text('Clicked!');
+    $(element).text('Added!');
     setTimeout(function() { $(element).text(element.id.split('-')[1]); }, 1000);
+    counter += parseInt(element.id.split('-')[1]);
+    $('#title').text('Total count: ' + counter.toString());
+  });
+  
+  $(document).on( 'click', '#reset', function () {
+    counter = 0;
+    $('#title').text('Add all the things!');
   });
 });
