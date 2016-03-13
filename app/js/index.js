41c9a633e03c0585fdc87c2c5f07c6b14027a393
Unai <unaimartinezrodriguez@gmail.com>
2016-03-13 22:40:27 +0100

    Action buttons for calc

--- app/js/index.js
+++ app/js/index.js
@@ -1,15 +1,30 @@
 $(function() {
   var counter = 0;
+  var status = 'add';
   $('.flex-container').on( 'click', '.flex-item', function () {
     var element = $(this).children()[0];
     $(element).text('Added!');
     setTimeout(function() { $(element).text(element.id.split('-')[1]); }, 1000);
-    counter += parseInt(element.id.split('-')[1]);
+    if (status == 'add') {
+      counter += parseInt(element.id.split('-')[1]);
+    } else {
+      counter -= parseInt(element.id.split('-')[1]);
+    }
     $('#title').text('Total count: ' + counter.toString());
   });
 
-  $(document).on( 'click', '#reset', function () {
+  $('.action-container').on( 'click', '#reset', function () {
     counter = 0;
     $('#title').text('Add all the things!');
+  });
+
+  $('.action-container').on( 'click', '#add', function () {
+    status = 'add';
+    $('#title').text('Add all the things!');
+  });
+
+  $('.action-container').on( 'click', '#substract', function () {
+    status = 'substract';
+    $('#title').text('Substract all the things!');
   });
 });
